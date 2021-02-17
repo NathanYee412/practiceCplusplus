ca103ba962c19fafd6cd34fe04674ffb90ea2878
Nathan Yee <32085568+NathanYee412@users.noreply.github.com>
2021-02-17 10:36:27 -0800

    Solved OddOccurencesInArray
    100%
    O(N) complexity

---
+++ OddOccurrencesInArray.h
@@ -0,0 +1,66 @@
+/*
+A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+
+For example, in array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the elements at indexes 0 and 2 have value 9,
+the elements at indexes 1 and 3 have value 3,
+the elements at indexes 4 and 6 have value 9,
+the element at index 5 has value 7 and is unpaired.
+Write a function:
+
+int solution(vector<int> &A);
+
+that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+
+For example, given array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the function should return 7, as explained in the example above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an odd integer within the range [1..1,000,000];
+each element of array A is an integer within the range [1..1,000,000,000];
+all but one of the values in A occur an even number of times.
+*/
+
+
+
+
+// you can use includes, for example:
+// #include <algorithm>
+#include<vector>
+#include<algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+
+    // copy vector 
+    vector<int> Acopy;
+    Acopy = A;
+
+    // sort vector
+    sort(Acopy.begin(), Acopy.end());
+
+    // size of the array
+    int size = Acopy.size();
+
+    // check sorted array if current position and next are the same
+    // if not return current element
+
+    for(int i = 0; i < size; i+=2) {
+        if(Acopy[i] != Acopy[i + 1]) {
+            return Acopy[i];
+        }
+    }
+        
+}
